59dfbdbcb000208a4562f57f752db50c4b4e8ada
isabelle <griff22i@mtholyoke.edu>
2017-11-30 23:20:33 -0500

    Update cheney-bot.js

--- cheney-bot.js
+++ cheney-bot.js
@@ -1,30 +1,14 @@
-var Discord = require('discord.io');
-var logger = require('winston');
+const Discord = require("discord.js");
+const cheney = new Discord.Client();
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
+cheney.on('ready', () => {
+  console.log(`Logged in as ${cheney.user.tag}!`);
 });
 
-logger.level = 'debug';
-// Initialize Discord Bot
-var cheney = new Discord.Client({
-   token: BOT_TOKEN,
-   autorun: true
+cheney.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-cheney.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(cheney.username + ' - (' + cheney.id + ')');
-});
-
-cheney.on('message', function(user, userID, channelID, message, event) {
-  logger.info('send message');
-  cheney.sendMessage({
-              to: channelID,
-              message: "Dick Cheney made money off the Iraq War.",
-              tts: true
-  });
-});
+cheney.login('process.env.BOT_TOKEN');
