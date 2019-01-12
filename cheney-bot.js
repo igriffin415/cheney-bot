73c82bec03d2e85a452626be857b8e9d768721d6
igriffin415 <fireice16@yahoo.com>
2019-01-12 00:37:45 -0500

    rstart?

--- cheney-bot.js
+++ cheney-bot.js
@@ -2,12 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    // console.log('I am ready!');
     client.user.setActivity('making money off the iraq war');
 });
 
 client.on('message', message => {
-  client.user.setActivity('making money off the iraq war');
   if(message.author != client.user){
     for (i = 0; i < 4; i++) {
       message.channel.send('Dick Cheney made money off the Iraq War.');
