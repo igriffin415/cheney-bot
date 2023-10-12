e96ac0229f07ac3b82711fcb8ecd6ed55d94b292
isabelle <23271503+igriffin415@users.noreply.github.com>
2023-10-12 19:11:12 -0400

    Update cheney-bot.js

--- cheney-bot.js
+++ cheney-bot.js
@@ -1,24 +1,26 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-client.on('ready', () => {
-  client.user.setPresence({
-    activities: [{
-      name: 'making $ off the iraq war'
-    }]
-  });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.once(Events.ClientReady, c => {
+	client.user.setActivity('making $ off the iraq war');
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('message', message => {
-  if (message.author != client.user) {
-    for (i = 0; i < 6; i++) {
-      message.channel.send({
-        content: 'Dick Cheney made money off the Iraq War.'
-      });
-    }
-  }
 
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+	
+	console.log(message.channelId)
+	
+	const channel = client.channels.cache.get(message.channelId);;
+
+	for(i = 0; i < 6; i++) {
+		channel.send('Dick Cheney made money off the Iraq war');
+	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+
+// Log in to Discord with your client's token
+client.login(token);
